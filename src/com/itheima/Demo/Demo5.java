e1292a40b2fbc2bd385ba3ddf6abcb034f1d325b
root <root@163.com>
2018-11-09 21:41:33 +0800

    提交到本地仓库

--- src/com/itheima/Demo/Demo5.java
+++ src/com/itheima/Demo/Demo5.java
@@ -1,5 +1,17 @@
 package com.itheima.Demo;
 
 public class Demo5 {
+    //方法一: 递归
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("第" + n + "个月，兔子的总数为" + fun(n));
+    }
 
+    private static int fun(int n) {
+        if ((1 == n) || (2 == n))
+            return 1;
+        else
+            return (fun(n - 1) + fun(n - 2));
+
+    }
 }
